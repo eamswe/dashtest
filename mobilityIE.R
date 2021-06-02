ee84f687ef70158e7d02ae5a525112141790195c
eamswe <83823963+eamswe@users.noreply.github.com>
2021-06-02 10:52:07 +0100

    Update mobilityIE.R

--- mobilityIE.R
+++ mobilityIE.R
@@ -18,3 +18,28 @@ mob_IE <- subset(mobility_IE,
 mob_IE$date <- as.Date(mob_IE$date , format = "%Y-%m-%d")
 
 write.csv(mob_IE[,c("date","retail_recreation","grocery_pharmacy", "public_transport", "workplaces", "residential" )], file="mobility.csv",row.names=TRUE)
+
+
+
+
+indeed <- read.csv(url("https://raw.githubusercontent.com/hiring-lab/data/master/IE/aggregate_job_postings_IE.csv"))
+indeed <-  spread(indeed, variable, pct_chng_feb_1)
+indeed$date <- as.Date(indeed$date , format = "%Y-%m-%d")
+indeed <- clean_names(indeed)
+write.csv(indeed, file="indeed_IE.csv",row.names=TRUE)
+
+
+url_cbi <- "https://www.centralbank.ie/docs/default-source/statistics/data-and-analysis/credit-and-banking-statistics/credit-and-debit-card-statistics/credit-and-debit-card-statistics-data/table-a-13-2-daily-card-payments-data.xlsx"
+cbi <- read.xlsx(url_cbi, startRow = 5)
+cbi$X1 <- convertToDate(cbi$X1, origin = "1900-01-01")
+cbi <- clean_names(cbi)
+cbi <- cbi %>% 
+  rename(
+    date = x1,
+  )
+cbi_total <- subset(cbi, select = c (date, spending_on_all_cards))  
+cbi_total <- cbi_total %>% mutate(spend_ma = zoo::rollmean(spending_on_all_cards, k = 7, fill = NA, align = c("right")))
+cbi_total <- cbi_total %>% mutate(cbi_index = (spend_ma/spend_ma[7])*100)
+cbi_total <-  cbi_total %>%
+  filter(date > "2020-03-06") 
+write.csv(cbi_total, file = "cbi_total_spending.csv", row.names =TRUE)
